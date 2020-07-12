b93a11990f10f0c3413bf9bb2e91e245ba2535c1
Lý Quang Thái <lyquangthai1993@gmail.com>
2020-07-12 09:56:31 +0700

    update profile

---
+++ precache-manifest.82ba77858e13be9f80d2112157ba72fd.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c283498c884c6c6e5e851953d31bdd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7e3848e1d0f5fe475f2a",
+    "url": "./static/css/2.85a921ec.chunk.css"
+  },
+  {
+    "revision": "f239648f45db4f20dc41",
+    "url": "./static/css/main.18b963f1.chunk.css"
+  },
+  {
+    "revision": "7e3848e1d0f5fe475f2a",
+    "url": "./static/js/2.f791af38.chunk.js"
+  },
+  {
+    "revision": "c0554f266784fabb0402ff1139c47fda",
+    "url": "./static/js/2.f791af38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f239648f45db4f20dc41",
+    "url": "./static/js/main.58737e4d.chunk.js"
+  },
+  {
+    "revision": "9a5e2af41b1d219c9bcd",
+    "url": "./static/js/runtime-main.25115752.js"
+  },
+  {
+    "revision": "8563342470ef03752fb0320ddf258eab",
+    "url": "./static/media/logo.85633424.png"
+  },
+  {
+    "revision": "1911662e7cae5908fb164bf414746c64",
+    "url": "./static/media/opensans-bold.1911662e.woff"
+  },
+  {
+    "revision": "298d5bec4cac463fed0121967236aef5",
+    "url": "./static/media/opensans-bold.298d5bec.eot"
+  },
+  {
+    "revision": "44a8b46648f43846d7afb6ad1927fedf",
+    "url": "./static/media/opensans-bold.44a8b466.woff2"
+  },
+  {
+    "revision": "7277d3caa25348119af34a25a1e93764",
+    "url": "./static/media/opensans-bold.7277d3ca.ttf"
+  },
+  {
+    "revision": "bdc0a4099e8acf70950026977b00e60c",
+    "url": "./static/media/opensans-bold.bdc0a409.svg"
+  },
+  {
+    "revision": "4af41748a43d8e22d0c4c5a89e334b55",
+    "url": "./static/media/opensans-italic.4af41748.woff2"
+  },
+  {
+    "revision": "847623a1d9487608aabdb83dd5b2584a",
+    "url": "./static/media/opensans-italic.847623a1.woff"
+  },
+  {
+    "revision": "ad63e4dfc9046d0c8ff7c3bd47531d76",
+    "url": "./static/media/opensans-italic.ad63e4df.eot"
+  },
+  {
+    "revision": "b98bebf3668c29ce2a00f179d6f2e708",
+    "url": "./static/media/opensans-italic.b98bebf3.ttf"
+  },
+  {
+    "revision": "f0ed289d7d4b14ba153eb87d89240eee",
+    "url": "./static/media/opensans-italic.f0ed289d.svg"
+  },
+  {
+    "revision": "147c7128fe2d0fbc55f574e8fad36f5d",
+    "url": "./static/media/opensans-light.147c7128.woff"
+  },
+  {
+    "revision": "3ecef1d6f3d0d2fcc6c801fc444d35bb",
+    "url": "./static/media/opensans-light.3ecef1d6.svg"
+  },
+  {
+    "revision": "95370e1d64931bbc5068084fa2e7c718",
+    "url": "./static/media/opensans-light.95370e1d.woff2"
+  },
+  {
+    "revision": "ab0cd793df2c2555272e7782298a862a",
+    "url": "./static/media/opensans-light.ab0cd793.ttf"
+  },
+  {
+    "revision": "b84fd90d85a67c30b8dff65875e18ad0",
+    "url": "./static/media/opensans-light.b84fd90d.eot"
+  },
+  {
+    "revision": "1dfce141e075bf6353a4c83fb46e36c4",
+    "url": "./static/media/opensans-regular.1dfce141.ttf"
+  },
+  {
+    "revision": "7c6cc2fe03a76e589d1a24a3e9a1293c",
+    "url": "./static/media/opensans-regular.7c6cc2fe.woff"
+  },
+  {
+    "revision": "90daec1bf4e0bfc9bd416611c3069623",
+    "url": "./static/media/opensans-regular.90daec1b.woff2"
+  },
+  {
+    "revision": "c2e6d81547cd1417be9b0bacbfac3ca0",
+    "url": "./static/media/opensans-regular.c2e6d815.svg"
+  },
+  {
+    "revision": "c686e6f7c6b4eb9a6f830ac93f7267f4",
+    "url": "./static/media/opensans-regular.c686e6f7.eot"
+  },
+  {
+    "revision": "5ebd7ac70d953a6a12837ddeb809b93f",
+    "url": "./static/media/opensans-semibold-webfont.5ebd7ac7.eot"
+  },
+  {
+    "revision": "8dc0c59a5887b3a156583f2aadb2e02f",
+    "url": "./static/media/opensans-semibold-webfont.8dc0c59a.woff"
+  },
+  {
+    "revision": "b5ee9a114b58520b11f7e7b3d3cc6708",
+    "url": "./static/media/opensans-semibold-webfont.b5ee9a11.woff2"
+  },
+  {
+    "revision": "c4a5b82959d9024a63d56135b9ce4fe7",
+    "url": "./static/media/opensans-semibold-webfont.c4a5b829.svg"
+  },
+  {
+    "revision": "390985c5aa33113a85b1ac818e6863bf",
+    "url": "./static/media/warning.390985c5.svg"
+  }
+]);
